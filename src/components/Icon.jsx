fd9610341cfc194ff8851f19307a2834d85150b2
kelio-mv <keliojosue2@gmail.com>
2023-12-29 11:44:53 -0300

    Added media queries for Icon and ProfilePicture

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,18 +1,18 @@
 function Icon(props) {
+  const filter = props.invert ? "invert(1)" : null;
   const highlightFilter =
     "brightness(0) saturate(100%) invert(55%) sepia(57%) saturate(617%) hue-rotate(124deg) brightness(95%) contrast(89%)";
-  const filter =
-    props.invert || matchMedia("(prefers-color-scheme: dark)").matches ? "invert(1)" : null;
 
   return (
     <img
       src={`/friendly/${props.name}.svg`}
+      className="icon"
       onClick={props.onClick}
       style={{
         filter: props.highlight ? highlightFilter : filter,
         display: props.inline ? "inline" : null,
         verticalAlign: props.inline ? "middle" : null,
-        opacity: props.dimmed || props.disabled ? 0.5 : 1,
+        opacity: props.dimmed || props.disabled ? 0.5 : null,
         cursor: props.onClick ? "pointer" : null,
         pointerEvents: props.disabled ? "none" : null,
       }}
--- src/components/ProfilePicture.jsx
+++ src/components/ProfilePicture.jsx
@@ -1,15 +1,14 @@
 function ProfilePicture(props) {
   const isDefault = ["default_avatar.png", "loading_pfp.png"].includes(props.src);
-  const isDarkMode = matchMedia("(prefers-color-scheme: dark)").matches;
 
   return (
     <img
       src={isDefault ? `/friendly/${props.src}` : props.src}
+      className={isDefault ? "profile-picture--default" : null}
       onClick={props.onClick}
       style={{
         width: props.size ? props.size : 72,
         borderRadius: "50%",
-        filter: isDefault && isDarkMode ? "invert(1)" : null,
         cursor: props.onClick ? "pointer" : null,
       }}
     />
@@ -18,7 +17,4 @@ function ProfilePicture(props) {
 
 export default ProfilePicture;
 
-/*
-The invert filter in Icon and ProfilePicture might be slowing down the rendering
-Once people vote whether to implement light mode or not, i'll decide what to do
-*/
+// The invert filter in Icon and ProfilePicture might be slowing down the rendering
