f4b6c1490bbcba9cb35e39ccd77ae51c04d685ad
kelio-mv <keliojosue2@gmail.com>
2024-01-11 19:10:45 -0300

    Updates

--- sw.js
+++ sw.js
@@ -1,32 +1,44 @@
-self.addEventListener("install", (e) => {
-  console.log("Installed");
-});
-
-self.addEventListener("activate", (e) => {
-  console.log("Activated");
-  e.waitUntil(clients.claim());
-});
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(handleRequest(e.request));
-});
+oninstall = skipWaiting;
+onactivate = (e) => e.waitUntil(clients.claim());
+onfetch = (e) => e.respondWith(handleRequest(e.request));
 
 async function handleRequest(req) {
   const match = await caches.match(req);
   if (match) return match;
   const res = await fetch(req);
-  putInCache(req, res.clone());
+  const clone = res.clone();
+  caches.open("friendly").then((cache) => cache.put(req, clone));
   return res;
 }
 
-async function putInCache(req, res) {
-  const cache = await caches.open("friendly");
-  await cache.put(req, res);
-}
-
-// if (match) return match;
-// else {
-//   const res = await fetch(req);
-//   const cache = await caches.open("friendly");
-//   await cache.put(req, res);
-// }
+// const images = [
+//   "default_avatar",
+//   "favicon",
+//   "loading_pfp",
+//   "logo",
+//   "menu_android",
+//   "menu_iphone",
+// ];
+// const icons = [
+//   "add",
+//   "alternate_email",
+//   "arrow_back",
+//   "article",
+//   "chat",
+//   "close",
+//   "contact_support",
+//   "delete",
+//   "delete_forever",
+//   "download",
+//   "favorite",
+//   "key",
+//   "logout",
+//   "menu",
+//   "person_book",
+//   "photo_camera",
+//   "send",
+//   "settings",
+//   "share",
+//   "visibility",
+//   "visibility_off",
+// ];
