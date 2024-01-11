befd25a772d256d7f6e2995689e629c257b4e726
kelio-mv <keliojosue2@gmail.com>
2024-01-11 20:02:59 -0300

    Updates

--- sw.js
+++ sw.js
@@ -1,6 +1,14 @@
-oninstall = (e) => e.waitUntil(skipWaiting());
+oninstall = (e) => {
+  e.waitUntil(addToCache());
+  skipWaiting();
+};
 onactivate = (e) => e.waitUntil(clients.claim());
 onfetch = (e) => e.respondWith(handleRequest(e.request));
+
+async function addToCache() {
+  const cache = await caches.open("friendly");
+  await cache.addAll(["index.html", "assets/index-fcc81b7c.js", "assets/index-16984635.css"]);
+}
 
 async function handleRequest(req) {
   const match = await caches.match(req);
