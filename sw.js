1d6d67b4dd9d5baf62362fc866402557f70a4895
kelio-mv <keliojosue2@gmail.com>
2024-01-11 15:08:24 -0300

    Updates

---
+++ sw.js
@@ -0,0 +1,32 @@
+self.addEventListener("install", (e) => {
+  console.log("Installed");
+});
+
+self.addEventListener("activate", (e) => {
+  console.log("Activated");
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(handleRequest(e.request));
+});
+
+async function handleRequest(req) {
+  const match = await caches.match(req);
+  if (match) return match;
+  const res = await fetch(req);
+  putInCache(req, res);
+  return res;
+}
+
+async function putInCache(req, res) {
+  const cache = await caches.open("friendly");
+  await cache.put(req, res.clone());
+}
+
+// if (match) return match;
+// else {
+//   const res = await fetch(req);
+//   const cache = await caches.open("friendly");
+//   await cache.put(req, res);
+// }
