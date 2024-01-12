1b73ca8f477941cb9737983e313cc060febe1cc1
kelio-mv <keliojosue2@gmail.com>
2024-01-12 12:25:03 -0300

    Updates

--- sw.js
+++ sw.js
@@ -1,21 +1,33 @@
-oninstall = (e) => {
-  e.waitUntil(addToCache());
-  skipWaiting();
+self.oninstall = () => skipWaiting();
+self.onactivate = (e) => e.waitUntil(clients.claim());
+self.onfetch = (e) => {
+  e.respondWith(
+    e.request.destination === "image" ? staleWhileRevalidate(e.request) : networkFirst(e.request)
+  );
 };
-onactivate = (e) => e.waitUntil(clients.claim());
-onfetch = (e) => e.respondWith(handleRequest(e.request));
 
-async function addToCache() {
-  const cache = await caches.open("friendly");
-  await cache.addAll(["index.html"]);
+async function staleWhileRevalidate(request) {
+  const cachedResponse = await caches.match(request);
+  if (cachedResponse) {
+    fetch(request).then((response) => store(request, response));
+    return cachedResponse;
+  }
+  const response = await fetch(request);
+  store(request, response.clone());
+  return response;
 }
 
-async function handleRequest(req) {
-  return fetch(req);
-  // const match = await caches.match(req);
-  // if (match) return match;
-  // const res = await fetch(req);
-  // const clone = res.clone();
-  // caches.open("friendly").then((cache) => cache.put(req, clone));
-  // return res;
+async function networkFirst(request) {
+  try {
+    const response = await fetch(request);
+    store(request, response.clone());
+    return response;
+  } catch {
+    console.log(request);
+    return caches.match(request);
+  }
+}
+
+function store(req, res) {
+  caches.open("friendly").then((cache) => cache.put(req, res));
 }
