136247a86547f4503a6f64732ed0606ab6767102
kelio-mv <keliojosue2@gmail.com>
2024-01-12 12:24:41 -0300

    Trying better strategies

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,33 @@
-oninstall = () => skipWaiting();
-onactivate = (e) => e.waitUntil(clients.claim());
-onfetch = (e) => e.respondWith(handleRequest(e.request));
+self.oninstall = () => skipWaiting();
+self.onactivate = (e) => e.waitUntil(clients.claim());
+self.onfetch = (e) => {
+  e.respondWith(
+    e.request.destination === "image" ? staleWhileRevalidate(e.request) : networkFirst(e.request)
+  );
+};
 
-async function handleRequest(request) {
+async function staleWhileRevalidate(request) {
   const cachedResponse = await caches.match(request);
-  if (cachedResponse) return cachedResponse;
-
+  if (cachedResponse) {
+    fetch(request).then((response) => store(request, response));
+    return cachedResponse;
+  }
   const response = await fetch(request);
-  const responseClone = response.clone();
-  caches.open("friendly").then((cache) => cache.put(request, responseClone));
+  store(request, response.clone());
   return response;
+}
+
+async function networkFirst(request) {
+  try {
+    const response = await fetch(request);
+    store(request, response.clone());
+    return response;
+  } catch {
+    console.log(request);
+    return caches.match(request);
+  }
+}
+
+function store(req, res) {
+  caches.open("friendly").then((cache) => cache.put(req, res));
 }
